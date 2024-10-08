04a45de560cfcc2d126ca22473f5415b4e3609ac
Caitlyn Wong <91009885+caitlynkywong@users.noreply.github.com>
2024-10-08 12:18:51 +1100

    Upload week05 code

---
+++ week05/part2_farm.c
@@ -0,0 +1,141 @@
+// Farmer's Farm
+// part1_farm.c
+//
+// This program was written by T11C
+// on 08/10/2024
+//
+// This program is a simple game that allows the user to build a farm.
+// The user can place barns, trees, and coops in the farm.
+
+#include <stdio.h>
+
+#define SIZE 5
+#define TREE_POINTS -10
+
+enum entity_type {
+    COOP,
+    BARN,
+    TREE,
+    FARMER,
+    EMPTY,
+};
+
+struct farm_entity {
+    enum entity_type entity;
+    int points;
+};
+
+struct player {
+    int row;
+    int col;
+};
+
+void print_farm(struct farm_entity farm[SIZE][SIZE]);
+void initialise_farm(struct farm_entity farm[SIZE][SIZE]);
+struct player place_farmer(struct farm_entity farm[SIZE][SIZE]);
+
+int main(void) {
+    struct farm_entity farm[SIZE][SIZE];
+
+    initialise_farm(farm);
+
+    place_farm_structures(farm);
+
+    struct player farmer = place_farmer(farm);
+
+    place_coops(farm);
+
+    print_farm(farm);
+
+    return 0;
+}
+
+// Function prints the map of the farm
+//
+// Parameters:
+// - farm: the 2D array representing the farm
+//
+// returns: nothing
+void print_farm(struct farm_entity farm[SIZE][SIZE]) {
+    printf("\n---------------------\n");
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("|");
+            if (farm[i][j].entity == FARMER) {
+                printf(" X ");
+            } else if (farm[i][j].entity == EMPTY) {
+                printf("   ");
+            } else if (farm[i][j].entity == COOP) {
+                printf(" o ");
+            } else if (farm[i][j].entity == BARN) {
+                printf(" # ");
+            } else if (farm[i][j].entity == TREE) {
+                printf(" T ");
+            }
+        }
+        printf("|\n");
+        printf("---------------------\n");
+    }
+}
+
+void initialise_farm(struct farm_entity farm[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            farm[i][j].entity = EMPTY;
+            farm[i][j].points = 0;
+        }
+    }
+}
+
+void place_farm_structures(struct farm_entity farm[SIZE][SIZE]) {
+    printf("Enter barns and trees:\n");
+
+    char type;
+    scanf(" %c", &type);
+    while (type != 'q') {
+        int row;
+        int col;
+        scanf(" %d %d", &row, &col);
+
+        if (type == 'b') {
+            int points;
+            scanf("%d", &points);
+            farm[row][col].entity = BARN;
+            farm[row][col].points = points;
+        } else if (type == 't') {
+            farm[row][col].entity = TREE;
+            farm[row][col].points = TREE_POINTS;
+        }
+        scanf(" %c", &type);
+    }
+}
+
+struct player place_farmer(struct farm_entity farm[SIZE][SIZE]) {
+    printf("Enter the starting position of the farmer: ");
+
+    struct player farmer;
+    scanf("%d %d", &farmer.row, &farmer.col);
+
+    while (farmer.row < 0 || farmer.row >= SIZE || farmer.col < 0 ||
+           farmer.col >= SIZE || farm[farmer.row][farmer.col].entity != EMPTY) {
+        printf("Invalid starting position!\n");
+        printf("Re-enter starting position: ");
+        scanf("%d %d", &farmer.row, &farmer.col);
+    }
+
+    farm[farmer.row][farmer.col].entity = FARMER;
+
+    return farmer;
+}
+
+void place_coops(struct farm_entity farm[SIZE][SIZE]) {
+    printf("Enter the position and points of the coop(s):\n");
+
+    int coop_row;
+    int coop_col;
+    int coop_points;
+    while (scanf("%d %d %d", &coop_row, &coop_col, &coop_points) == 3) {
+        farm[coop_row][coop_col].entity = COOP;
+        farm[coop_row][coop_col].points = coop_points;
+    }
+}
---
+++ week05/part3_simple_string.c
@@ -0,0 +1,62 @@
+// part3_simple_string.c
+//
+// This program was written by Sofia De Bellis (z5418801)
+// on March 2024
+//
+// This program demonstrates how to work with strings in C.
+
+
+#include <stdio.h>
+
+#define MAX_SIZE 1024
+
+int main(void) {
+    // Declare and initialise a string
+    char my_string[] = "hello world!";
+    
+    // Traverse the string and print each character
+    printf("Traverse the string and print each character using a while loop:\n");
+    int i = 0;
+    while (my_string[i] != '\0') {
+        printf("%c", my_string[i]);
+        i++;
+    }
+    printf("\n\n");
+
+    // Another way to traverse the string and print each character
+    printf("Traverse the string and print each character using a for loop:\n");
+    for (int i = 0; my_string[i] != '\0'; i++) {
+        printf("%c", my_string[i]);
+    }
+    printf("\n\n");
+
+    // How to print a string in its entirety
+    printf("Print a string in its entirety using printf():\n");
+    printf("My string: %s\n\n", my_string);
+
+
+    // Declare a string
+    char another_string[MAX_SIZE];
+    
+    // Read a string from the user, note we do NOT use scanf for strings
+    printf("Enter a string: ");
+    fgets(another_string, MAX_SIZE, stdin);
+    
+    // Print the string using fputs
+    printf("Print the string using fputs:\n");
+    fputs(another_string, stdout);
+    printf("\n");
+
+    // Print the string one character at a time
+    printf("Print the string one character at a time:\n");
+    for (int i = 0; another_string[i] != '\0'; i++) {
+        if (another_string[i] == '\n') {
+            printf("\\n");
+        } else {
+            printf("%c,", another_string[i]);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
